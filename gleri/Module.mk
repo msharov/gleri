62002aa5b6b936785a5c8031ae891584914d0ae2
Mike Sharov <msharov@users.sourceforge.net>
2013-01-30 18:14:08 -0500

    Install targets for the library and headers

--- gleri/Module.mk
+++ gleri/Module.mk
@@ -3,6 +3,7 @@
 LIBA	:= .o/gleri/libgleri.a
 LIBSRC	:= $(wildcard gleri/*.cc)
 LIBOBJ	:= $(addprefix .o/,$(LIBSRC:.cc=.o))
+LIBINC	:= gleri.h $(wildcard gleri/*.h)
 LIBDEPS	:= ${LIBOBJ:.o=.d}
 
 ################ Compilation ###########################################
@@ -13,6 +14,40 @@ ${LIBA}: ${LIBOBJ}
 	@${AR} qc $@ ${LIBOBJ}
 
 ${LIBOBJ}:	Makefile gleri/Module.mk ${CONFS}
+
+################ Installation ##########################################
+
+.PHONY:	uninstall-headers uninstall-lib
+
+##### Install headers
+
+ifdef INCDIR	# These ifdefs allow cold bootstrap
+INCSI	:= $(addprefix ${INCDIR}/,${LIBINC})
+
+install:	${INCSI}
+${INCSI}: ${INCDIR}/%.h: %.h
+	@echo "Installing $@ ..."
+	@${INSTALLDATA} $< $@
+uninstall:	uninstall-headers
+uninstall-headers:
+	@echo "Removing gleri headers ..."
+	@(cd ${INCDIR}; rm -f ${INCSI}; [ ! -d gleri ] || rm -rf gleri)
+endif
+
+##### Install library
+
+ifdef LIBDIR
+LIBAI	:= ${LIBDIR}/$(notdir ${LIBA})
+install:	${LIBAI}
+${LIBAI}:	${LIBA}
+	@echo "Installing $@ ..."
+	@${INSTALLLIB} $< $@
+
+uninstall:	uninstall-lib
+uninstall-lib:
+	@echo "Removing library from ${LIBDIR} ..."
+	@rm -f ${LIBAI}
+endif
 
 ################ Maintenance ###########################################
 
