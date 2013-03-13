b07c7b71d239f1962f8720e37a22da15204de0aa
Mike Sharov <msharov@users.sourceforge.net>
2013-03-13 15:27:59 -0400

    Fix RGLR dirty trick

--- gleri/rglrp.cc
+++ gleri/rglrp.cc
@@ -5,8 +5,9 @@
 
 #include "rglrp.h"
 
-#define N(n,s)	"\0" #n "\0" #s "\0"
+#define N(n,s)	#n "\0" #s "\0"
 /*static*/ const char PRGLR::_cmdNames[] =
+     "\0"	// Dirty trick: object name is 4 bytes, but must be zero terminated
      N(GLError,s)
      N(Restate,(qqqqyyyy))
      N(Expose,)
@@ -16,17 +17,17 @@
 
 /*static*/ inline const char* PRGLR::LookupCmdName (ECmd cmd, size_type& sz) noexcept
 {
-    return (CCmdBuf::LookupCmdName((unsigned)cmd,sz,ArrayBlock(_cmdNames)-1));
+    return (CCmdBuf::LookupCmdName((unsigned)cmd,sz,_cmdNames+1,sizeof(_cmdNames)-2));
 }
 
 /*static*/ PRGLR::ECmd PRGLR::LookupCmd (const char* name, size_type bleft) noexcept
 {
-    return (ECmd(CCmdBuf::LookupCmd(name,bleft,ArrayBlock(_cmdNames)-1)));
+    return (ECmd(CCmdBuf::LookupCmd(name+1,bleft,_cmdNames+1,sizeof(_cmdNames)-2)));
 }
 
 bstro PRGLR::CreateCmd (ECmd cmd, size_type sz) noexcept
 {
     size_type msz;
-    const char* m = LookupCmdName (cmd, msz);
-    return (CCmdBuf::CreateCmd (RGLRObject, m, msz, sz));
+    const char* m = LookupCmdName(cmd, msz);
+    return (CCmdBuf::CreateCmd (RGLRObject, m-1, msz+1, sz));
 }
